bfbd91a1133f27d59e1825f136380dbe5df58c4a
Elisio Mualumene <elisio.mualumene@nzooji.com>
2022-10-06 21:39:43 +0100

    :hammer: navbar done

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const root = document.querySelector(".root")
+
+root.innerHTML = 
+`
+<nav class="menu">
+    <h1>Develop | Shop</h1>
+    <ul>
+        <a href="#"><li>Shop</li></a>
+        <a href="#"><li>Produtos</li></a>
+        <a href="#"><li>Categorias</li></a>
+        <a href="#"><li>Contato</li></a>
+    </ul>
+
+</nav>
+
+<div class="head">
+    <p>A Develop é uma marca de Roupa Angola, que tem por objetivo Proporcionar a melhor qualidade para os seus clientes.</p>
+    <img src="" alt="header image"/>
+</div>
+`
+
